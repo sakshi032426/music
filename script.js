efed04737f92a24fed2304d9eec9aa88d54afbf8
sakshi032426 <121580963+sakshi032426@users.noreply.github.com>
2023-08-10 11:11:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,8 @@ document.getElementById('next').addEventListener('click', ()=>{
     else{
         songIndex += 1;
     }
-    audioElement.src = `songs/${songIndex+1}.mp3`;
+   audioElement.src = `${songIndex+1}.mp3`;
+
     masterSongName.innerText = songs[songIndex].songName;
     audioElement.currentTime = 0;
     audioElement.play();
